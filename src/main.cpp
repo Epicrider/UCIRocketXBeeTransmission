ffef7ec92eabf1ff5b0be881e9ff2b5c630bf357
Epicrider <ohossain@uci.edu>
2022-04-24 11:14:26 -0700

    Send Basic Message with Software Serial Output

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial XBee(2,3);
+// SoftwareSerial XBee(0,1);
+int count = 0;
 
 void setup()
 {
-  Serial.begin(9600);
-  XBee.begin(9600);
+  // Serial1 XBee
+  Serial1.begin(115200);
+  // Serial Virtual
+  Serial.begin(115200);
+
+  // XBee.setSerial();
+  // Do we need to begin XBee?
+  // XBee.begin(9600);
+
+
 }
 
 void loop()
 {
   // Send the message:
-  Serial.print("Send Char");
-  XBee.write('1');
+  Serial.print(String(count) + ": Sending Message through Serial1...\n");
+  Serial1.print("Hello" + String(count));
+  count++;
   delay(1000);
+
+  // XBee.write('1');
 }
